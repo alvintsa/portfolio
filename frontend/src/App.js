4ba5a3324a7eace89a1fa8c79987f759eee37d42
Alvin <alvintsa@buffalo.edu>
2024-04-22 14:31:36 -0400

    added frontned routes and homepage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,33 @@
 // src/App.js
 import React from 'react';
-import Projects from './components/Projects';
-import Test from "./components/Test";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import Projects from './components/Projects';
+import Layout from "./pages/Layout";
+import Home from "./pages/Home";
+import Blogs from "./pages/Blogs";
+import Contact from "./pages/Contact";
+import NoPage from "./pages/NoPage";
+import Projects from "./pages/Projects";
 
 const App = () => {
     return (
-        <div>
-            <h1>My Portfolio</h1>
-            {/* <Projects /> */}
-            <Test />
-        </div>
+        <BrowserRouter>
+            <div>
+                <h1>My Portfolio</h1>
+                {/* Define your routes */}
+                <Routes>
+                    <Route path="/" element={<Layout />}>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/projects" element={<Projects />} />
+                        <Route path="/blogs" element={<Blogs />} />
+                        <Route path="/contact" element={<Contact />} />
+                        <Route path="*" element={<NoPage />} />
+                    </Route>
+                </Routes>
+                
+                {/* <Projects /> */}
+            </div>
+        </BrowserRouter>
     );
 };
 
---
+++ frontend/src/pages/Projects.js
@@ -0,0 +1,5 @@
+const Contact = () => {
+    return <h1>My Projects</h1>;
+  };
+  
+  export default Contact;
