68103dff1455898ab00bc2a507d980209a352b21
Alvin <alvintsa@buffalo.edu>
2024-04-22 15:14:14 -0400

    setting up contact form

---
+++ frontend/src/components/ContactForm.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const ContactForm = () => {
+    //TODO: CONTACT FORM (THIS WILL SHOW UP ON THE BOTTOM RIGHT OF EVERY PAGE!!) NOT TO BE CONFUSED WITH THE 
+    // TODO: CONTACT COMPONENT IN PAGES DIRECTORY
+    //TODO:
+    return (
+        <form>
+            <label>
+                
+            </label>
+
+        </form>
+
+
+    );
+
+
+};
+
+export default ContactForm;
